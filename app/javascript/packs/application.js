9fa255991fa206eb7c26bd3d4e2ed28e9ebbab10
f0rest8 <markthayer@hey.com>
2020-07-28 15:09:47 -0700

    Added sidebar initial styling.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("trix")
+require("@rails/actiontext")
 
 import "stylesheets/application";
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -16,5 +18,3 @@ import "stylesheets/application";
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-require("trix")
-require("@rails/actiontext")
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :pwned_password
+
+  validates_uniqueness_of :username
+  validates_presence_of :username
 end
