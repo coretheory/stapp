df4ee6654df9e10d4c76512ab069172c626e9349
f0rest8 <markthayer@hey.com>
2020-07-31 17:13:20 -0700

    Fixed clickedOutside vue js on dropdowns.

--- app/javascript/components/dropdown.vue
+++ app/javascript/components/dropdown.vue
@@ -1,21 +1,29 @@
 <template>
-	<div class="relative inline-block">
-		<div role="button" class="inline-block select-none" @click.prevent="open = !open">
-			<slot name="link"></slot>
-		</div>
+  <div v-click-outside="clickedOutside" class="relative inline-block">
 
-		<div v-show="open" class="absolute pin-r w-32 mt-2 z-10">
-			<slot name="dropdown-items"></slot>
-		</div>
-	</div>
+    <div role="button" class="inline-block select-none" @click.prevent="open = !open">
+      <slot name="link"></slot>
+    </div>
+
+    <div v-show="open" class="absolute pin-r mt-2 z-10">
+      <slot name="dropdown-items"></slot>
+    </div>
+
+  </div>
 </template>
 
 <script>
 export default {
-	data () {
-		return {
-			open: false,
-		}
-	}
+  data() {
+    return {
+      open: false,
+    }
+  },
+  
+  methods: {
+    clickedOutside() {
+      this.open = false
+    }
+  },
 }
 </script>
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,14 +22,36 @@ import "components/direct_uploads"
 import TurbolinksAdapter from 'vue-turbolinks'
 import Vue from 'vue/dist/vue.esm'
 import App from '../app.vue'
+import Dropdown from '../components/dropdown'
 
 Vue.use(TurbolinksAdapter)
 
-import Dropdown from '../components/dropdown'
 Vue.component('dropdown', Dropdown)
 
+
+// Directive for handling clicks outside of the element.
+// We use this to automatically close the dropdown if you
+// click somewhere else on the page.
+Vue.directive('click-outside', {
+  bind: function (el, binding, vNode) {
+    el.__vueClickOutside__ = event => {
+      if (!el.contains(event.target)) {
+        // call method provided in v-click-outside value
+        vNode.context[binding.expression](event)
+        event.stopPropagation()
+      }
+    }
+    document.body.addEventListener('click', el.__vueClickOutside__)
+  },
+  unbind: function (el, binding, vNode) {
+    // Remove Event Listeners
+    document.removeEventListener('click', el.__vueClickOutside__)
+    el.__vueClickOutside__ = null
+  }
+})
+
 document.addEventListener('turbolinks:load', () => {
- const app = new Vue({
-   el: "[data-behavior='vue']",
- })
+  const app = new Vue({
+    el: "[data-behavior='vue']",
+  })
 })
