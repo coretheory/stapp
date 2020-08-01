8efd58327a2260e12ded466c478951b730401c4d
f0rest8 <markthayer@hey.com>
2020-08-01 15:07:31 -0700

    Sidebar stylings (need collapsible projects menu now.)

--- app/javascript/components/dropdown.vue
+++ app/javascript/components/dropdown.vue
@@ -1,11 +1,11 @@
 <template>
   <div v-click-outside="clickedOutside" class="relative flex flex-wrap inline-block">
 
-    <div role="button" class="inline-block flex-1 select-none" @click.prevent="open = !open">
+    <div role="button" class="relative inline-block flex-1 select-none" @click.prevent="open = !open">
       <slot name="link"></slot>
     </div>
 
-    <div v-show="open" class="absolute pin-r mt-2 z-10">
+    <div v-show="open" class="absolute w-full pin-r mt-2 pt-4 z-10">
       <slot name="dropdown-items"></slot>
     </div>
 
