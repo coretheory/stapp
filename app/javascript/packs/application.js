e90e40d81c451717f48f0e3af5daec24d3ac8cf0
f0rest8 <markthayer@hey.com>
2020-08-01 21:21:16 -0700

    Removed vue, ready for stimulus.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,48 +12,10 @@ require("@rails/actiontext")
 
 import "stylesheets/application"
 import "components/direct_uploads"
+import "controllers"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-import TurbolinksAdapter from 'vue-turbolinks'
-import Vue from 'vue/dist/vue.esm'
-import App from '../app.vue'
-import Dropdown from '../components/dropdown'
-
-Vue.use(TurbolinksAdapter)
-
-Vue.component('dropdown', Dropdown)
-
-
-// Directive for handling clicks outside of the element.
-// We use this to automatically close the dropdown if you
-// click somewhere else on the page.
-Vue.directive('click-outside', {
-  bind: function (el, binding, vNode) {
-    el.__vueClickOutside__ = event => {
-      if (!el.contains(event.target)) {
-        // call method provided in v-click-outside value
-        vNode.context[binding.expression](event)
-        event.stopPropagation()
-      }
-    }
-    document.body.addEventListener('click', el.__vueClickOutside__)
-  },
-  unbind: function (el, binding, vNode) {
-    // Remove Event Listeners
-    document.removeEventListener('click', el.__vueClickOutside__)
-    el.__vueClickOutside__ = null
-  }
-})
-
-document.addEventListener('turbolinks:load', () => {
-  const app = new Vue({
-    el: "[data-behavior='vue']",
-  })
-})
-
-import "controllers"
