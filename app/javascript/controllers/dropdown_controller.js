53cc29fbc2add7bd9598577b6420ff90096c3dd5
f0rest8 <markthayer@hey.com>
2020-08-02 12:57:47 -0700

    Stimulus successfully added and vue removed.

---
+++ app/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,8 @@
+// src/controllers/dropdown_controller.js
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+	connect() {
+    console.log("hello from StimulusJS")
+  }
+}
