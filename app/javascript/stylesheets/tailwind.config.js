b24912feeaeaa8885e51a9e92616ce1125bc31c3
f0rest8 <markthayer@hey.com>
2020-08-04 14:12:06 -0700

    Styling in progress.

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -245,6 +245,8 @@ module.exports = {
         '"Segoe UI Emoji"',
         '"Segoe UI Symbol"',
         '"Noto Color Emoji"',
+        'din-condensed',
+        'acumin-pro',
       ],
       serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
       mono: ['Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
