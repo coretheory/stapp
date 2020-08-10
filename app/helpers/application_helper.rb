fffe8b2ba367b293177057b8dd927fe6ed462141
f0rest8 <markthayer@hey.com>
2020-08-10 10:47:13 -0700

    Refactor page titles for index, about, contact

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,13 @@ module ApplicationHelper
     title ||= name.underscore.humanize
     inline_svg_tag(filename, aria: true, nocomment: true, title: title, class: styles)
   end
+
+  def full_title(page_title = '')
+  	base_title = "SARA THAYER"
+  	if page_title.empty?
+  		base_title
+  	else
+  		page_title + " | " + base_title
+  	end
+  end
 end
