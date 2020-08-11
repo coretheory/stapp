ef1d7a9f07dbce4f6844e49e1cc947b5084f9227
f0rest8 <markthayer@hey.com>
2020-08-11 10:57:01 -0700

    Update admin check

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,11 @@ Rails.application.routes.draw do
   
   devise_for :users
   resources :users, only: [:show], as: "profile"
+
+  authenticate :user, lambda { |u| u.admin? } do
+    namespace :admin do
+      resources :users
+      root to: "users#index"
+    end
+  end
 end
