d4e05db0c00d4f0b7eab48972f251c00176197ca
f0rest8 <markthayer@hey.com>
2020-08-12 13:13:17 -0700

    Remove initializer/put in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,15 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
+  ActionMailer::Base.smtp_settings = {
+    user_name: "<%= Rails.application.credentials.sendgrid[:sg_username] %>",
+    password: "<%= Rails.application.credentials.sendgrid[:sg_password] %>",
+    domain: "<%= Rails.application.credentials.sendgrid[:sg_domain] %>",
+    address: "<%= Rails.application.credentials.sendgrid[:sg_address] %>",
+    port: "<%= Rails.application.credentials.sendgrid[:sg_port] %>",
+    authentication: "<%= Rails.application.credentials.sendgrid[:sg_authentication] %>",
+    enable_starttls_auto: "<%= Rails.application.credentials.sendgrid[:sg_enable_starttls_auto] %>"
+  }
   
 
   # Ignore bad email addresses and do not raise email delivery errors.
--- config/initializers/smtp.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-    user_name: "<%= Rails.application.credentials.sendgrid[:sg_username] %>",
-    password: "<%= Rails.application.credentials.sendgrid[:sg_password] %>",
-    domain: "<%= Rails.application.credentials.sendgrid[:sg_domain] %>",
-    address: "<%= Rails.application.credentials.sendgrid[:sg_address] %>",
-    port: "<%= Rails.application.credentials.sendgrid[:sg_port] %>",
-    authentication: "<%= Rails.application.credentials.sendgrid[:sg_authentication] %>",
-    enable_starttls_auto: "<%= Rails.application.credentials.sendgrid[:sg_enable_starttls_auto] %>"
-  }
