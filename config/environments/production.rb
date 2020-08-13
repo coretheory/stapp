5f9731073d86889c4860186b5100b8fee074da1d
f0rest8 <markthayer@hey.com>
2020-08-12 20:57:13 -0700

    Update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
     domain: 'sarathayer.com',
     address: 'smtp.sendgrid.net',
     port: '587',
-    authentication: 'plain',
+    authentication: :plain,
     enable_starttls_auto: true
   }
 
