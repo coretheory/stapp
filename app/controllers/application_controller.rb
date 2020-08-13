8886aac21a78c344aba73eedacdcc9cc8be33965
f0rest8 <markthayer@hey.com>
2020-08-13 16:46:15 -0700

    Fix for friendly id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
       format.json { render nothing: true, status: :not_found }
-      format.html { redirect_to root_path, alert: 'Feature coming this future.' }
+      format.html { redirect_to root_path, alert: 'Feature possibly coming in the future.' }
       format.js   { render nothing: true, status: :not_found }
     end
   end
