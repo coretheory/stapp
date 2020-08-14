fb048e681daef69bf16a9d954d710f3bf0febbf7
f0rest8 <markthayer@hey.com>
2020-08-13 20:21:09 -0700

    Fix hcaptcha

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,11 +6,11 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
     @contact.request = request
-    if verify_hcpatcha(message: "Don't forget the hCaptcha!") && @contact.deliver
+    if verify_hcaptcha(secret_key: Rails.application.credentials.hcaptcha[:HCAPTCHA_SECRET_KEY], message: "Don't forget the hCaptcha!") && @contact.deliver
       flash.now[:error] = nil
   		redirect_to root_path, notice: 'Message sent successfully'
     else
-      flash.now[:error] = 'Cannot send message'
+      flash.now[:error] = "Uh oh! Can't send that message. Did you complete the hCaptcha?"
       render :new
     end
   end
