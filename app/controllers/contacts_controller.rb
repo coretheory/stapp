4b076d3ee8b8df8b7965e1011ed96996e5f7d0c3
f0rest8 <markthayer@hey.com>
2020-08-13 20:33:34 -0700

    Update success message

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,7 @@ class ContactsController < ApplicationController
     @contact.request = request
     if verify_hcaptcha(secret_key: Rails.application.credentials.hcaptcha[:HCAPTCHA_SECRET_KEY], message: "Don't forget the hCaptcha!") && @contact.deliver
       flash.now[:error] = nil
-  		redirect_to root_path, notice: 'Message sent successfully'
+  		redirect_to root_path, notice: 'Message sent successfully, thank you.'
     else
       flash.now[:error] = "Uh oh! Can't send that message. Did you complete the hCaptcha?"
       render :new
