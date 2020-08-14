c6b989f474fba9eee14c3fdc360cc0c4527af972
f0rest8 <markthayer@hey.com>
2020-08-14 13:06:34 -0700

    Change to symbol

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,9 +6,9 @@ class Contact < MailForm::Base
 
   def headers
     {
-      :subject => "New Contact Submission from #{name} | SARATHAYER.COM",
+      :subject => "New Contact Submission from #{:name} | SARATHAYER.COM",
       :to => "hello@sarathayer.com",
-      :from => %("#{name}" <#{email}>)
+      :from => %("#{:name}" <#{:email}>)
     }
   end
 end
