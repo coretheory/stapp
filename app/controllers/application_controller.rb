4c04a5bc802001a2669deb7c260b9e07a8fbaeba
f0rest8 <markthayer@hey.com>
2020-08-14 17:56:57 -0700

    Add initial english/spanish translations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action set_locale
-	before_action :configure_permitted_parameters, if: :devise_controller?
+	before_action :set_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
@@ -16,13 +16,21 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    I18n.
+    if user_signed_in?
+      I18n.locale = current_user.language
+    else
+      I18n.locale = params[:lang] || locale_from_header || I18n.default_locale
+    end
+  end
+
+  def locale_from_header
+    request.env.fetch('HTTP_ACCEPT_LANGUAGE', '').scan(/[a-z]{2}/).first
   end
 
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:username] # this can grow to however many fields you need
+    added_attrs = [:username, :language] # this can grow to however many fields you need
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_032432) do
+ActiveRecord::Schema.define(version: 2020_08_14_230319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,6 +106,7 @@ ActiveRecord::Schema.define(version: 2020_08_13_032432) do
     t.string "username"
     t.boolean "admin", default: false
     t.string "slug"
+    t.string "language"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
