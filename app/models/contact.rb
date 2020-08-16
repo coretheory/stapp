a10998a273ba6840d5b1b42d9ebf87ebb3bc8b52
f0rest8 <markthayer@hey.com>
2020-08-15 18:54:24 -0700

    Refactor contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,19 +1,18 @@
 class Contact < MailForm::Base
+  VALID_NAME_REGEX = /\A[a-z ,.'-]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
   attribute :first_name
-  validates :first_name, presence: true, length: { minimum: 2, maximum: 25}
-  validates_format_of :first_name, with: /\A[a-z ,.'-]+\z/i
+  validates :first_name, presence: true, length: { minimum: 2, maximum: 25}, format: { with: VALID_NAME_REGEX }
 
   attribute :last_name
-  validates :last_name, presence: true, length: { minimum: 2, maximum: 25}
-  validates_format_of :last_name, with: /\A[a-z ,.'-]+\z/i
+  validates :last_name, presence: true, length: { minimum: 2, maximum: 25}, format: { with: VALID_NAME_REGEX }
 
   attribute :email
-  validates :email, presence: true, length: { maximum: 250 }
-  validates_format_of :email, with: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  validates :email, presence: true, length: { maximum: 250 }, format: { with: VALID_EMAIL_REGEX }
 
   attribute :company
-  validates :company, presence: true, length: { minimum: 2, maximum: 50 }
-  validates_format_of :company, with: /\A[a-z ,.'-]+\z/i
+  validates :company, presence: true, length: { minimum: 2, maximum: 50 }, format: { with: VALID_NAME_REGEX }
 
   attribute :message
   validates :message, presence: true, length: { minimum: 20, maximum: 2000}
