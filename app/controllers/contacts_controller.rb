0ed98cea5b0076170deb570b5c502d94ec3e2e1c
f0rest8 <markthayer@hey.com>
2020-08-15 17:06:44 -0700

    Update contact form validation and style

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,7 +10,7 @@ class ContactsController < ApplicationController
       flash.now[:error] = nil
   		redirect_to root_path, notice: 'Thank you for your message. We will contact you soon!'
     else
-      flash.now[:error] = "Cannot send message. Did you complete the hCaptcha?"
+      flash.now[:error] = "Woops, cannot send that message. Please try again."
       render :new
     end
   end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,12 +1,28 @@
 class Contact < MailForm::Base
-  attribute :name, :validate => true
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :message,   :validate => true
-  attribute :nickname,  :captcha  => true
+  attribute :first_name
+  validates :first_name, presence: true, length: { minimum: 2, maximum: 25}
+  validates_format_of :first_name, with: /\A[a-z ,.'-]+\z/i
+
+  attribute :last_name
+  validates :last_name, presence: true, length: { minimum: 2, maximum: 25}
+  validates_format_of :last_name, with: /\A[a-z ,.'-]+\z/i
+
+  attribute :email
+  validates :email, presence: true, length: { maximum: 250 }
+  validates_format_of :email, with: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+
+  attribute :company
+  validates :company, presence: true, length: { minimum: 2, maximum: 50 }
+  validates_format_of :company, with: /\A[a-z ,.'-]+\z/i
+
+  attribute :message
+  validates :message, presence: true, length: { minimum: 20, maximum: 2000}
+
+  attribute :nickname, :captcha  => true
 
   def headers
     {
-      :subject => "New Contact from #{name} <#{email}> | SARATHAYER.COM",
+      :subject => "New Contact from #{first_name} #{last_name} <#{email}> @ #{company} | SARATHAYER.COM",
       :to => "hello@sarathayer.com",
       :from => "mailbot@sarathayer.com"
     }
