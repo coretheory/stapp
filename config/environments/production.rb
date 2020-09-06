88a3db271048d7f3a2372612d97a5dac231d9f1c
f0rest8 <markthayer@hey.com>
2020-09-05 23:21:59 -0700

    Update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.default_url_options = { host: 'www.sarathayer.com', protocol: 'https' }
 
   Rails.application.routes.default_url_options[:host] = 'www.sarathayer.com'
