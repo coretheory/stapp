d2fa1bdf1086b05da6fb4c4a80140a71faf4928f
f0rest8 <markthayer@hey.com>
2020-09-05 23:45:57 -0700

    Add heroku database url to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,15 +69,10 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "stapp_production"
 
   config.action_mailer.perform_caching = false
-
   config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.perform_deliveries = true
-
-  config.action_mailer.default_url_options = { host: 'www.sarathayer.com', protocol: 'https' }
-
-  Rails.application.routes.default_url_options[:host] = 'www.sarathayer.com'
-
+  host = 'www.sarathayer.com'
+  config.action_mailer.default_url_options = { host: host }
+  Rails.application.routes.default_url_options[:host] = host
   ActionMailer::Base.smtp_settings = {
     user_name: Rails.application.credentials.sendgrid[:SENDGRID_USERNAME],
     password: Rails.application.credentials.sendgrid[:SENDGRID_PASSWORD],
@@ -90,7 +85,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
