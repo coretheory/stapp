e53b8ec15def14b1b3d613a7e442b18aef46e33c
f0rest8 <markthayer@hey.com>
2020-09-05 23:13:37 -0700

    Add delivery errors to see what's going on

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,13 +69,26 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "stapp_production"
 
   config.action_mailer.perform_caching = false
+
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'www.sarathayer.com' }
+
+  config.action_mailer.default_url_options = { host: 'www.sarathayer.com', protocol: 'https' }
+
   Rails.application.routes.default_url_options[:host] = 'www.sarathayer.com'
+
+  ActionMailer::Base.smtp_settings = {
+    user_name: Rails.application.credentials.sendgrid[:SENDGRID_USERNAME],
+    password: Rails.application.credentials.sendgrid[:SENDGRID_PASSWORD],
+    domain: 'sarathayer.com',
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
--- config/initializers/smtp.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-    user_name: Rails.application.credentials.sendgrid[:SENDGRID_USERNAME],
-    password: Rails.application.credentials.sendgrid[:SENDGRID_PASSWORD],
-    domain: 'sarathayer.com',
-    address: 'smtp.sendgrid.net',
-    port: '587',
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
