1d3905a27988d26e184bd04f489a93a85d102ab9
f0rest8 <markthayer@hey.com>
2020-09-06 23:42:17 -0700

    Update

--- app/mailers/audience_mailer.rb
+++ app/mailers/audience_mailer.rb
@@ -12,7 +12,8 @@ class AudienceMailer < ApplicationMailer
   end
 
   def new_post_notification
+    @post = params[:post]
 
-    mail(to: @notification.recipient.email, subject: "New post by Sara @ sarathayer.com")
+    mail(to: @post.recipient, subject: "New post by Sara @ sarathayer.com")
   end
 end
